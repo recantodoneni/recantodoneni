e3d56e7d3c024e5b0285b4e4f4bf58bf7add0ed0
jcblank <jcarlos.x@gmail.com>
2024-01-29 14:45:26 -0300

    Ajustado layout responsivo

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ let wp = document.getElementById("whats");
 // Seleciona o id date no html
 let date = document.getElementById("date");
 let p = document.getElementById("point");
+let ev = document.getElementById("event");
+
 // Variável que possui o link do aibnb
 let linkAB = '<a href="https://www.airbnb.com/h/recantodoneni" target="_blank"><img src="airbnb.png" alt="airbnb">Faça a sua reserva</a>';
 // Variável que possui o link do whatsapp
 let linkW = '<a href="https://wa.me/5551992887129/?" target="_blank"><img src="whatsapp.png" alt="whatsapp">Fale conosco</a>';
 // Variável que chama a data
 let point = '<a href="https://www.santamariadoherval.rs.gov.br/index.php/turismo/pontos-turisticos/570" target="_blank"><img src="" alt="">Pontos Turísticos</a>'
+
+
+let evt = '<a href="https://www.santamariadoherval.rs.gov.br/eventos" target="_blank"><img src="" alt="">Eventos</a>'
+
+
 let d = new Date()
 // Variável que concatena o ano com o texto do rodapé
 let recanto = `Recanto do Neni. ` + d.getFullYear() + ` Todos os direitos reservados©.`;
@@ -21,5 +28,6 @@ ab.innerHTML = linkAB
 // Link whats
 wp.innerHTML = linkW
 p.innerHTML = point
+ev.innerHTML = evt
 // Texto + ano rodapé
 date.innerHTML = recanto;
