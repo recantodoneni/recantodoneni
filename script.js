3b6792aa6213205c4db2f7e8bc0c900f8a027aba
jcblank <jcarlos.x@gmail.com>
2024-01-29 11:32:12 -0300

    Inserido links e redirecionamento para nova aba

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+// Seleciona o id aibnb no html
+let ab = document.getElementById("airbnb");
+// Seleciona o id whats no html
+let wp = document.getElementById("whats");
+// Seleciona o id date no html
 let date = document.getElementById("date");
+// Variável que possui o link do aibnb
+let linkAB = '<a href="https://www.airbnb.com/h/recantodoneni" target="_blank"><img src="airbnb.png" alt="airbnb">Faça a sua reserva</a>';
+// Variável que possui o link do whatsapp
+let linkW = '<a href="https://wa.me/5551992887129/?" target="_blank"><img src="whatsapp.png" alt="whatsapp">Fale conosco</a>';
+// Variável que chama a data
 let d = new Date()
-
-date.innerHTML = `Recanto do Neni. ` + d.getFullYear() + ` Todos os direitos reservados ©.` 
+// Variável que concatena o ano com o texto do rodapé
+let recanto = `Recanto do Neni. ` + d.getFullYear() + ` Todos os direitos reservados©.`;
+// Insere as informações no html
+// Link airbnb
+ab.innerHTML = linkAB
+// Link whats
+wp.innerHTML = linkW
+// Texto + ano rodapé
+date.innerHTML = recanto;
