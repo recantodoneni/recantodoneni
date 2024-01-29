7c536b87131ad6a3e9449d5da6455ccb8722dc4e
jcblank <jcarlos.x@gmail.com>
2024-01-29 15:24:14 -0300

    Adicionado ícones nos botões 'Eventos' e 'Localização'

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ let wp = document.getElementById("whats");
 let date = document.getElementById("date");
 let p = document.getElementById("point");
 let ev = document.getElementById("event");
+let lo = document.getElementById("loc");
 
 // Variável que possui o link do aibnb
 let linkAB = '<a href="https://www.airbnb.com/h/recantodoneni" target="_blank"><img src="airbnb.png" alt="airbnb">Faça a sua reserva</a>';
 // Variável que possui o link do whatsapp
 let linkW = '<a href="https://wa.me/5551992887129/?" target="_blank"><img src="whatsapp.png" alt="whatsapp">Fale conosco</a>';
 // Variável que chama a data
-let point = '<a href="https://www.santamariadoherval.rs.gov.br/index.php/turismo/pontos-turisticos/570" target="_blank"><img src="" alt="">Pontos Turísticos</a>'
+let local = '<a href="https://maps.app.goo.gl/uqn7LPWrEzZ5YzVFA" target="_blank"><img src="localizacao.png" alt="">Localização</a>';
+let point = '<a href="https://www.santamariadoherval.rs.gov.br/index.php/turismo/pontos-turisticos/570" target="_blank"><img src="camera.png" alt="">Pontos Turísticos</a>'
 
 
-let evt = '<a href="https://www.santamariadoherval.rs.gov.br/eventos" target="_blank"><img src="" alt="">Eventos</a>'
+let evt = '<a href="https://www.santamariadoherval.rs.gov.br/eventos" target="_blank"><img src="calendar.png" alt="">Eventos</a>'
 
 
 let d = new Date()
@@ -27,6 +29,7 @@ let recanto = `Recanto do Neni. ` + d.getFullYear() + ` Todos os direitos reserv
 ab.innerHTML = linkAB
 // Link whats
 wp.innerHTML = linkW
+lo.innerHTML = local
 p.innerHTML = point
 ev.innerHTML = evt
 // Texto + ano rodapé
