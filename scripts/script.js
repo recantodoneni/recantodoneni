a46de8651e2793f0874f5d2fd47ed398f7c292c8
jcblank <jcarlos.x@gmail.com>
2024-02-14 17:23:12 -0300

    Inserido aba 'onde comer' ajustado css e js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
+let comer = document.getElementById("co")
+let c123 = document.getElementById("co1") 
+let cl = document.getElementById("close")
+
+comer.addEventListener("click", function comer() { 
+    c123.style.display = "flex"
+})
+
+cl.addEventListener("click", function close(){
+    c123.style.display = "none"
+})
+
 let d = new Date()
 // Variável que concatena o ano com o texto do rodapé
 let recanto = `Recanto do Neni. ` + d.getFullYear() + ` Todos os direitos reservados©.`;
